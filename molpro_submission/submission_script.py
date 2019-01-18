c5b4c34e738280b560a4135815cdaaa38717b3f7
Silvia Amabilino <walfits@gmail.com>
2019-01-18 10:08:28 +0000

    Fixed problem with trajectory index being too large

--- molpro_submission/submission_script.py
+++ molpro_submission/submission_script.py
@@ -9,6 +9,7 @@ import sys
 ###### Things to change #############
 path_to_molpro_inputs = "/Volumes/Transcend/useful_scripts/molpro_submission/cn_isopentane_molpro_inputs_dft"
 indices_to_run = list(range(int(sys.argv[1]),int(sys.argv[2])))
+key_in_filename = "ccsd"
 #####################################
 
 filenames = glob.glob(path_to_molpro_inputs + "/*.com")
@@ -27,17 +28,14 @@ for file in filenames_to_run:
     file_name_with_extension = file.split("/")[-1]
 
     for line in submit_script:
-        if "b3lyp_tzvp_u" in line:
-            index_end = line.find("b3lyp_tzvp_u")
+        if "ccsd" in line:
+            index_end = line.find(key_in_filename)
             index_start = index_end - 8
-            line = line.replace(line[index_start:index_end], file_name_with_extension[:8])
+            line = line.replace(line[index_start:index_end], file_name_with_extension[:len(key_in_filename)+4])
         temp_submit_script.write(line)
 
     temp_submit_script.close()
     submit_script.close()
 
     os.system("qsub submit.sh")
-
-
-os.system("python gather_results.py")
 
